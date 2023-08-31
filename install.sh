ad715a10ee2d98e38f6ade5cf374d4d57370ac68
coco-server <cocobird231@gmail.com>
2023-08-31 12:21:47 +0800

    Update v1.1-dev codePack. Modifies Dockerfile and install.sh to support qos saving.

--- install.sh
+++ install.sh
@@ -342,7 +342,7 @@ InstallDocker()
     cat source_env.txt >> run.sh
     sudo chmod a+x run.sh
     # Add docker run process
-    echo "sudo docker run -v ~/ros2_docker/codePack/$pack_name/launch/common.yaml:/ros2_ws/install/$pack_name/share/$pack_name/launch/common.yaml --rm --privileged --net host -it ros2_docker ros2 launch $pack_name launch.py" >> run.sh
+    echo "sudo docker run -v ~/ros2_docker/codePack/$pack_name/launch/common.yaml:/ros2_ws/install/$pack_name/share/$pack_name/launch/common.yaml ~/ros2_docker/launch/qos:/ros2_ws/launch/qos --rm --privileged --net host -it ros2_docker ros2 launch $pack_name launch.py" >> run.sh
 
     # Install Dockerfile Process
     echo "Installing dockerfile..."
